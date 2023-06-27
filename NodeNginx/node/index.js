7cd3f523531adc87205407bbdae7e57877c90306
Thiago Felipe <thiagof.g3@gmail.com>
2023-06-27 20:22:58 -0300

    feat: salvando no banco toda vez que requisitar

--- NodeNginx/node/index.js
+++ NodeNginx/node/index.js
@@ -1,9 +1,38 @@
 const express = require('express')
+const mysql = require('mysql')
+
 const app = express()
 const port = 3001
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle AQUI!!!</h1>')
+    const connection = mysql.createConnection(config)
+    const query = `INSERT INTO people(name) values('Thiago')`;
+    connection.query(query);
+
+    let htmlList = '';
+
+    connection.query('SELECT * FROM people', (error, results, fields) => {
+        if (error) throw error;
+
+        results.forEach(people => {
+            htmlList += `<li>${people.name}</li>`
+        });
+
+        res.send(`
+            <h1>Full Cycle AQUI!!!</h1>
+            <ol>
+                ${htmlList}
+            </ol>
+        `);
+    });
+
+    connection.end();
 })
 
 app.listen(port, () => {
